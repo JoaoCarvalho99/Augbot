a0115d66e48275623c6095f1f3d42fe94d53b108
JoaoCarvalho99 <joaopdemcarvalho@gmail.com>
2022-07-06 15:40:36 +0100

    Obstacle Detection node

--- scripts/AlphaBot2/python/Infrared_Obstacle_Detection.py
+++ scripts/AlphaBot2/python/Infrared_Obstacle_Detection.py
@@ -1,38 +1,48 @@
+#!/usr/bin/env python3
+import rospy
 import RPi.GPIO as GPIO
 import time
 from AlphaBot2 import AlphaBot2
 
-Ab = AlphaBot2()
+from Augbot.msg import synchPoint
 
 DR = 16
 DL = 19
+BUZ = 5
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(DR,GPIO.IN,GPIO.PUD_UP)
-GPIO.setup(DL,GPIO.IN,GPIO.PUD_UP)
+def beep_on():
+	GPIO.output(BUZ,GPIO.HIGH)
+def beep_off():
+	GPIO.output(BUZ,GPIO.LOW)
 
-try:
-	while True:
-		DR_status = GPIO.input(DR)
-		DL_status = GPIO.input(DL)
-#		print(DR_status,DL_status)
-		sleeptime = 0
-		if(DL_status == 0):
-			print ("DL")
-			sleeptime = 0.002
-		if (DR_status == 0):
-			print ("DR")
-			sleeptime = 0.002
-#			Ab.left()
-			#Ab.right()
-		time.sleep(sleeptime)
-#			Ab.stop()
-		#	print("object")
-#		else:
-#			Ab.forward()
-		#	print("forward")
+def detect():
+	Ab = AlphaBot2()
 
-except KeyboardInterrupt:
-	GPIO.cleanup();
 
+
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setwarnings(False)
+	GPIO.setup(DR,GPIO.IN,GPIO.PUD_UP)
+	GPIO.setup(DL,GPIO.IN,GPIO.PUD_UP)
+	GPIO.setup(BUZ,GPIO.OUT)
+
+	try:
+		while True:
+			DR_status = GPIO.input(DR)
+			DL_status = GPIO.input(DL)
+			if(DL_status == 0) or (DR_status == 0):
+				pub.publish ( synchPoint( round(time.time() * 1000) ) )
+				beep_on()
+				time.sleep(0.01)
+				beep_off()
+				time.sleep(5)
+
+	except KeyboardInterrupt:
+		GPIO.cleanup()
+
+if __name__ == '__main__':
+
+    pub = rospy.Publisher('synchPoints', synchPoint, queue_size=1)
+    rospy.init_node('obstacleDetetion', anonymous=True)
+   
+    detect()
